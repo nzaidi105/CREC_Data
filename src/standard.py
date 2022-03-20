e9c991307047b4e51178f8f045090a7fef5cee61
skanner2 <63942471+skanner2@users.noreply.github.com>
2022-03-19 21:59:04 -0400

    added to script

--- src/standard.py
+++ src/standard.py
@@ -4,3 +4,16 @@ from pathlib import Path
 import pandas as pd
 
 
+raw = Path('data/raw')
+
+path_to_file = os.path.join(raw,'CxI_DR_2007_2012_PRO_DET.xlsx')
+
+df = pd.read_excel(path_to_file, sheet_name='2012')
+
+if __name__ == '__main__':
+
+    #change code to first 4 characters
+    df['Code'] = df['Code'].str[:4]
+    print(df.head())
+    
+
