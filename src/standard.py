031b2610cfe306dbe50f58537f782d04ad100ebc
skanner2 <63942471+skanner2@users.noreply.github.com>
2022-03-26 17:01:47 -0400

    fixed missing values

--- src/standard.py
+++ src/standard.py
@@ -16,10 +16,11 @@ if __name__ == '__main__':
     df = pd.read_excel(path_to_file, sheet_name='2012')
     #change code to first 4 characters
     df['Code'] = df['Code'].str[:4]
+    #fixes missing values
+    df.ffill(inplace=True)
     output = os.path.join(standard, file_name)
     #output
     df.to_excel(output)
-    
     print(df.head())
     
 
